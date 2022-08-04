a65dde04948967f95056b9fb8442fd93ca4886c4
ybjv02 <88575461+ybjv02@users.noreply.github.com>
2022-08-04 14:44:11 +0800

    edited background

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ function calculateOrdDate() {
     // get client input
     let batch = document.getElementById("batch").value;
     let year = document.getElementById("year").value;
-    // validate input (type & range check)
 
+    // validate input (type & range check)
     if (batch.length == 0 || year.length == 0 || isNaN(batch) || isNaN(year) || parseInt(batch) < 1 || parseInt(batch) > 500 || ![3, 4].includes(parseInt(year))) {
-        document.getElementById("date").innerHTML = "Invalid<br>Input";
+        document.getElementById("ord-date").innerHTML = "Invalid<br>Input";
         return;
     }
+
     // calculate ord date based on batch & year
     let ordDate;
     if (batch % 2 != 0) {
@@ -19,12 +20,13 @@ function calculateOrdDate() {
         let addYears = Math.floor(batch / 2 - 1) + (year - 3);
         ordDate = new Date(2022 + addYears, 3, 5);
     }
+
     // update number of days till ord
     let currentDate = new Date();
     let daysTillOrd = Math.floor((ordDate.getTime() - currentDate.getTime()) / (1000 * 3600 * 24));
     if (daysTillOrd > 0) {
-        document.getElementById("date").innerHTML = "You ORD in<br>" + daysTillOrd + " days";
+        document.getElementById("ord-date").innerHTML = "You ORD in<br>" + daysTillOrd + " days";
     } else {
-        document.getElementById("date").innerHTML = "ORD LO!";
+        document.getElementById("ord-date").innerHTML = "ORD LO!";
     }
 }
